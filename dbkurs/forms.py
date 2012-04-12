f3ce12473e28651beffe6909112af37a7c85103b
Orekhov Artemy <lawbv@yandex.ru>
2012-04-12 23:53:42 +0300

    new stuff

---
+++ dbkurs/forms.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django import forms
+from dbkurs import util
+import psycopg2
+
+class OrderForm(forms.Form):
+    conn=psycopg2.connect(util.pgset())
+    cur=conn.cursor()
+    reqmes='Поле не должно быть пустым'
+    query = "SELECT diagram_id,vehicle,agent,plan_date FROM delivery_diagrams "
+    query+= " WHERE status=false ORDER BY plan_date asc;"
+    cur.execute(query)
+    info=cur.fetchall()
+    info_ok= [(0,'Выберите график доставки'),]
+    for el in info:
+        v=str(el[3])+" - "+el[1]+" - "+el[2]
+        info_ok.append((el[0],v),)
+    diagram_id = forms.ChoiceField(label='График доставки',choices=info_ok)
+    
+    query = "SELECT customer_id,name FROM customers ORDER BY name asc;"
+    cur.execute(query)
+    info=cur.fetchall()
+    info_ok= [(0,'Выберите клиента'),]
+    for el in info:
+        info_ok.append((el[0],el[1]),)
+    customer_id = forms.ChoiceField(label='Клиент',choices=info_ok)
+
+    query = "SELECT delpoint_id,del_address FROM delpoints ORDER BY del_address asc;"
+    cur.execute(query)
+    info=cur.fetchall()
+    info_ok= [(0,'Выберите пункт доставки'),]
+    for el in info:
+        info_ok.append((el[0],el[1]),)
+    delpoint_id = forms.ChoiceField(label='Пункт доставки',choices=info_ok)
+    
+    responsible = forms.CharField(label='Ответственный менеджер',error_messages={'required': reqmes})
+    
+    query = "SELECT output_id,output_name FROM outputs ORDER BY output_name asc;"
+    cur.execute(query)
+    outputs=cur.fetchall()
+    outputs_ok = [(0,'Выберите товар'),]
+    for el in outputs:
+        outputs_ok.append((el[0],el[1]),)
+    #ch=[('M','Male'),('F','Female')]
+    output_id1 = forms.ChoiceField(label='Товар 1',choices=outputs_ok)
+    output_id1q= forms.IntegerField(label='Количество, шт',error_messages={'required': reqmes})
+    output_id1d= forms.IntegerField(label='Скидка, %',error_messages={'required': reqmes})
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    # is invoked automatically by Django because of it starts with clean_
+    # when form.is_valid() is invoked
+    def clean_diagram_id(self):
+        di=self.cleaned_data['diagram_id']
+        if di=='0':
+            raise forms.ValidationError("График не выбран")
+        return di
+    
+    def clean_customer_id(self):
+        ci=self.cleaned_data['customer_id']
+        if ci=='0':
+            raise forms.ValidationError("Клиент не выбран")
+        return ci
+        
+    def clean_delpoint_id(self):
+        di=self.cleaned_data['delpoint_id']
+        if di=='0':
+            raise forms.ValidationError("Пункт не выбран")
+        return di
+    
+    def clean_output_id1(self):
+        oi=self.cleaned_data['output_id1']
+        if oi=='0':
+            raise forms.ValidationError("Товар не выбран")
+        return oi
+    
+    def clean_responsible(self):
+        ri=self.cleaned_data['responsible']
+        if (';' in ri)or(')' in ri)or(',' in ri):
+            raise forms.ValidationError("Недопустимые символы")
+        return ri
+    
+    def clean_output_id1q(self):
+        cd=self.cleaned_data['output_id1q']
+        if int(cd) not in range(1,51):
+            raise forms.ValidationError("Значение вне допустимого")
+        return cd
+           
+    def clean_output_id1d(self):
+        cd=self.cleaned_data['output_id1d']
+        if int(cd) not in range(0,91):
+            raise forms.ValidationError("Значение вне допустимого")
+        return cd
+        
+        
+class AddCustomerForm(forms.Form):
+    conn=psycopg2.connect(util.pgset())
+    cur=conn.cursor()
+    reqmes='Поле не должно быть пустым'
+    name = forms.CharField(label='ФИО',error_messages={'required':reqmes })
+    address = forms.CharField(label='Адрес',error_messages={'required': reqmes})
+    phone = forms.CharField(label='Телефон',error_messages={'required': reqmes})
+    fax = forms.CharField(label='Факс',error_messages={'required': reqmes})
+    email = forms.CharField(label='E-mail',error_messages={'required': reqmes})
+    type = forms.ChoiceField(label='Тип',choices=[('1','Физическое лицо'),('2','Юридическое лицо')])
+
+    bank = forms.CharField(label='Банк',required=False)
+    account = forms.IntegerField(label='Аккаунт',required=False)
+    bik = forms.IntegerField(label='БИК',required=False)
+    inn = forms.IntegerField(label='ИНН',required=False)
+    okonh = forms.IntegerField(label='ОКОНХ',required=False)
+    okpo = forms.IntegerField(label='ОКПО',required=False)
+    
+    
+class AddOutputForm(forms.Form):
+    conn=psycopg2.connect(util.pgset())
+    cur=conn.cursor()
+    reqmes='Поле не должно быть пустым'
+    output_name = forms.CharField(label='Название',error_messages={'required':reqmes })
+    output_price = forms.IntegerField(label='Стоимость',error_messages={'required': reqmes})
--- dbkurs/views.py
+++ dbkurs/views.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from django.shortcuts import HttpResponseRedirect
 from dbkurs import util
+from dbkurs.forms import OrderForm, AddCustomerForm, AddOutputForm
 import psycopg2
 
 def mainp(request):
@@ -10,24 +14,41 @@ def mcss(request):
     
 def customers(request):
     data = False
-    if 'ctype' in request.GET:
+    if 'ctype' in request.GET: # equals if request was
         data = True
-        query = "SELECT customer_id, name, "
-        infoheader=['id','Name']
-        if 'showopt' in request.GET:
-            so=request.GET.getlist('showopt')
-            for h in so:
-                query+=str(h)
-                infoheader.append(str(h).capitalize())
-                query+=", "
-        query = query[:-2]
-        ct=request.GET['ctype']
-        if ct=='0':
-            query+= " FROM customers;"
+        ct=request.GET['ctype'] # what is chosen?
+        if ct=='0' or ct=='2': # if 'all' have chosen
+            query = "SELECT customer_id, name, "
+            infoheader=['id','Name']
+            if 'showopt' in request.GET: #if any checkbox is selected
+                so=request.GET.getlist('showopt')
+                for h in so: # for each selected checkbox
+                    str_h=str(h)
+                    if str_h not in('bank','account','bik','inn','okonh','okpo'):
+                        query+=str_h+", "
+                        infoheader.append(str(h).capitalize())
+            query = query[:-2]
+            if ct=='0':
+                query+= " FROM customers;"
+            elif ct=='2':
+                query+= " FROM customers WHERE type=FALSE;"
+            
         elif ct=='1':
-            query+= " FROM customers WHERE type=FALSE;"
-        elif ct=='2':
-            query+= " FROM customers WHERE type=TRUE;"
+            query = "SELECT customers.customer_id, customers.name, "
+            infoheader=['id','Name']
+            if 'showopt' in request.GET: #if any checkbox is selected
+                so=request.GET.getlist('showopt')
+                for h in so: # for each selected checkbox
+                    str_h=str(h)
+                    infoheader.append(str(h).capitalize())
+                    if str_h not in('bank','account','bik','inn','okonh','okpo'):
+                        query+="customers."+str_h+", "
+                    else:
+                        query+="customers_lp."+str_h+", "
+            query = query[:-2]
+            query+= " FROM customers RIGHT JOIN customers_lp "
+            query+= " on(customers.customer_id=customers_lp.customer_id);"
+            
         conn=psycopg2.connect(util.pgset())
         cur=conn.cursor()
         cur.execute(query)
@@ -90,6 +111,86 @@ def orders(request):
     cur.close()
     conn.close()
     return render_to_response('orders.html',locals())
+
+def addorder(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            diagram_id = str(cd['diagram_id'])
+            customer_id =str(cd['customer_id'])
+            delpoint_id =str(cd['delpoint_id'])
+            responsible =cd['responsible']
+            output_id1 = str(cd['output_id1'])
+            output_id1q = int(cd['output_id1q'])
+            output_id1d = int(cd['output_id1d'])
+            conn=psycopg2.connect(util.pgset())
+            cur=conn.cursor()  
+            
+            # calculating order_amount and order_total
+            squery="SELECT output_price FROM outputs "
+            squery+=" WHERE output_id="+output_id1+";"
+            cur.execute(squery)
+            info=cur.fetchone()
+            order_amount=str(int(info[0])*output_id1q)
+            order_total=str(int(order_amount)*(100-output_id1d)/100)
+            cur.execute("SELECT CURRENT_DATE;")
+            info=cur.fetchone()
+            order_date=str(info[0])
+            
+            query="INSERT into orders(diagram_id,customer_id,delpoint_id,"
+            query+="responsible,order_date,order_amount,order_total) "
+            query+=" VALUES("+diagram_id+","+customer_id+","+delpoint_id
+            query+=",\'"+responsible+"\','"+order_date+"',"+order_amount+","+order_total+");"
+            cur.execute(query)
+            conn.commit()
+            
+            cur.execute("SELECT max(order_id) FROM orders")
+            oid=(cur.fetchone())[0]
+            
+            # works only for 1 output in order
+            discount=int(order_amount)-int(order_total)
+            query2="INSERT into orders_outputs(order_id,output_id,quantity,discount) values"
+            query2+=" ("+str(oid)+","+str(output_id1)+","+str(output_id1q)+","+str(discount)+");"
+            cur.execute(query2)
+            conn.commit()
+            
+            
+            cur.close()
+            conn.close()
+            
+            return render_to_response('thanks.html',locals())
+            # use this below after DEBUD
+            #return HttpResponseRedirect('thanks')
+        return render_to_response('addorder.html',locals())
+    else:
+        form = OrderForm()
+        return render_to_response('addorder.html',locals())
+
+def addcustomer(request):
+    if request.method == 'POST':
+        form = AddCustomerForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            return HttpResponseRedirect('thanks')
+        return render_to_response('addcustomer.html',locals())
+    else:
+        form = AddCustomerForm()
+        return render_to_response('addcustomer.html',locals())
+    
+def addoutput(request):
+    if request.method == 'POST':
+        form = AddOutputForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            return HttpResponseRedirect('thanks')
+        return render_to_response('addoutput.html',locals())
+    else:
+        form = AddOutputForm()
+        return render_to_response('addoutput.html',locals())
+
+def thanks(request):
+    return render_to_response('thanks.html')
 
 def ocss(request):
     return render_to_response('other.css')
